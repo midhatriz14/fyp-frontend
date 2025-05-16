e3e76bfa96763a2dfa94277f95a245bb117a62e7
alirizvi512 <syedalisajjadrizvi72@gmail.com>
2025-05-17 02:44:51 +0500

    Committing Delete and Edit Package

--- services/updatePackage.ts
+++ services/updatePackage.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export default async function updatePackage(packageId: string, updatedData: any) {
-    // const url = `http://13.233.214.252:3000/vendor/package/${packageId}`;
-    const url = `http://192.168.100.15:3000/vendor/package/${packageId}`;
+    const url = `http://13.233.214.252:3000/vendor/package/${packageId}`;
+    // const url = `http://192.168.100.15:3000/vendor/package/${packageId}`;
 
     const config: AxiosRequestConfig = {
         method: "PATCH",
